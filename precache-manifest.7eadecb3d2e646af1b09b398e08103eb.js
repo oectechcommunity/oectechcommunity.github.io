fa5914780f8741e1b2c185291835c150179c6e02
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2020-09-29 20:58:36 +0530

    Updates

---
+++ precache-manifest.7eadecb3d2e646af1b09b398e08103eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f9c9d11f241150dfe9635e18fba1b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "596f410e9207dc9b2318",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "9e2624c1c16d6f896c6b",
+    "url": "/static/css/main.07eaae1b.chunk.css"
+  },
+  {
+    "revision": "596f410e9207dc9b2318",
+    "url": "/static/js/2.c159ebe4.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/static/js/2.c159ebe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2624c1c16d6f896c6b",
+    "url": "/static/js/main.6f0fa4a8.chunk.js"
+  },
+  {
+    "revision": "104a82d6ed29e430dd90",
+    "url": "/static/js/runtime-main.a6cd9d73.js"
+  },
+  {
+    "revision": "280454a570b654a309800bfd23f6e405",
+    "url": "/static/media/logo.280454a5.png"
+  },
+  {
+    "revision": "48633ea82a1f222601de228b62cee939",
+    "url": "/static/media/pankaj-kumar-mondal.48633ea8.jpg"
+  },
+  {
+    "revision": "191232d6a9f8e0f9d7c0ca4a19e7eb7d",
+    "url": "/static/media/soh2018.191232d6.jpg"
+  },
+  {
+    "revision": "26fbe47a806ddc0fe35c0839a647c428",
+    "url": "/static/media/team-work.26fbe47a.svg"
+  }
+]);
